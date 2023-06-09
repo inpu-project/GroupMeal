5fba2b8ecf3c367fffec1ec72dc6ec7ee76ff4e1
hyjun2352 <khyj030502@gmail.com>
2023-06-09 18:25:31 +0900

    asdfdsa

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.use((req, res, next) => {
 
 
 // 프로필 페이지
-router.get('/profile', isLoggedIn, (req, res) => {
+router.get('/profile', (req, res) => {
     res.render('profile', { title: '내 정보'});
 });
 
