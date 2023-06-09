f5e64513865ab7abfa590c820500d6e0a26b5498
danielredglasses <sk091204@gmail.com>
2023-06-09 17:58:17 +0900

    add signup.js

--- public/signup.js
+++ public/signup.js
@@ -1,81 +1,26 @@
+const submit_btn = document.getElementById('submit_btn');
+const password = document.getElementById('password');
+const password_confirm = document.getElementById('password_confirm');
+password_confirm.onkeyup = function() {
+    update_btn_disabled();
+};
+
 function onclick_keyword(event) {
     event.classList.toggle("selected_keyword_pink");
     console.log(event.childNodes[1]);
     event.childNodes[1].value = event.classList.contains("selected_keyword_pink") ? "true" : "false";
+    update_btn_disabled();
 }
-
-// let nickname, email, gender, age, password, password_confirm;
-// let nickname_error, email_error, password_error, password_confirm_error, personalities_error;
-
-// window.onload = () => {
-//     init_settings();
-// }
-
-// function init_settings() {
-//     nickname = document.getElementById('nickname');
-//     email = document.getElementById('email');
-//     gender = document.getElementById('gender');
-//     age = document.getElementById('age');
-//     password = document.getElementById('password');
-//     password_confirm = document.getElementById('password_confirm');
-
-//     nickname_error = document.getElementById('nickname_error');
-//     email_error = document.getElementById('email_error');
-//     password_error = document.getElementById('password_error');
-//     password_confirm_error = document.getElementById('password_confirm_error');
-//     personalities_error = document.getElementById('personalities_error');
-// }
-
-// function possible_nickname(nickname) {
-//     var url = 'http://localhost:8001/auth/check_nickname';
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.open("POST", url);
-//     xhttp.setRequestHeader("Content-Type", "application/json");
-//     xhttp.send(JSON.stringify({nickname: nickname}));
-// }
-
-// function check_requirements() {
-
-//     // check nickname
-//     nickname_error.style.display = 'none';
-//     if(!possible_nickname(nickname.value)) {
-//         nickname_error.value = '사용할 수 없는 닉네임입니다';
-//         nickname_error.style.display = 'block';
-//         return;
-//     }
-//     else {
-//         nickname_error.value = '사용가능한 닉네임입니다';
-//     }
-
-//     // check password
-//     password_error.style.display = 'none';
-//     if(password.value != password_confirm.value) {
-//         password_error.value = '비밀번호가 일치하지 않습니다';
-//         password_error.style.display = 'block';
-//         return;
-//     }
-
-//     // check personality
-//     const personalities = document.querySelectorAll('li');
-//     let selected_personalities = [];
-//     let selected_cnt = 0;
-//     for(let i = 0; i < personalities.length; i++) {
-//         if(personalities[i].classList.contains('selected_keyword_red')) {
-//             selected_personalities.push(1);
-//             selected_cnt++;
-//         }
-//         else selected_personalities.push(0);
-//     }
-//     const personalities_error = document.getElementById('personalities_error');
-//     personalities_error.style.display = 'none';
-//     if(selected_cnt === 0) {
-//         personalities_error.value = '성격 키워드를 골라주세요';
-//         personalities_error.style.display = 'block';
-//         return;
-//     }
-//     else if(selected_cnt > 2) {
-//         personalities_error.value = '성격 키워드를 최대 2개 골라주세요';
-//         personalities_error.style.display = 'block';
-//         return;
-//     }
-// }
+function update_btn_disabled() {
+    console.log(password.value, password_confirm.value);
+    submit_btn.disabled = true;
+    if(password.value.trim() == 0 || password_confirm.value.trim() == 0) return;
+    if(password.value != password_confirm.value) return;
+    console.log("password is the same");
+    const personalities = document.querySelectorAll('li');
+    let keyword_cnt = 0;
+    for(let i = 0; i < personalities.length; i++) if(personalities[i].classList.contains('selected_keyword_pink')) keyword_cnt++;
+    if(keyword_cnt == 0 || keyword_cnt > 2) return;
+    console.log("keyword_cnt is fine");
+    submit_btn.disabled = false;
+}
