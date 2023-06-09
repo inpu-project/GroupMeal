e70368e2aae715db7a2c870ea1be3dba72bf419e
danielredglasses <sk091204@gmail.com>
2023-06-09 10:15:21 +0900

    signup page

--- public/signup.js
+++ public/signup.js
@@ -1,3 +1,25 @@
+let nickname, email, gender, age, password, password_confirm;
+let nickname_error, email_error, password_error, password_confirm_error, personalities_error;
+
+window.onload = () => {
+    init_settings();
+}
+
+function init_settings() {
+    nickname = document.getElementById('nickname');
+    email = document.getElementById('email');
+    gender = document.getElementById('gender');
+    age = document.getElementById('age');
+    password = document.getElementById('password');
+    password_confirm = document.getElementById('password_confirm');
+
+    nickname_error = document.getElementById('nickname_error');
+    email_error = document.getElementById('email_error');
+    password_error = document.getElementById('password_error');
+    password_confirm_error = document.getElementById('password_confirm_error');
+    personalities_error = document.getElementById('personalities_error');
+}
+
 function possible_nickname(nickname) {
     var url = 'http://localhost:8001/auth/check_nickname';
     var xhttp = new XMLHttpRequest();
@@ -7,17 +29,10 @@ function possible_nickname(nickname) {
 }
 
 function check_requirements() {
-    const nickname = document.getElementById('nickname').value;
-    const email = document.getElementById('email').value;
-    const gender = document.getElementById('gender').value;
-    const age = document.getElementById('age').value;
-    const password = document.getElementById('password').value;
-    const password_confirm = document.getElementById('password_confirm').value;
 
     // check nickname
-    const nickname_error = document.getElementById('nickname_error');
     nickname_error.style.display = 'none';
-    if(!possible_nickname(nickname)) {
+    if(!possible_nickname(nickname.value)) {
         nickname_error.value = '사용할 수 없는 닉네임입니다';
         nickname_error.style.display = 'block';
         return;
@@ -27,20 +42,19 @@ function check_requirements() {
     }
 
     // check password
-    const password_error = document.getElementById('password_error');
     password_error.style.display = 'none';
-    if(password != password_confirm) {
+    if(password.value != password_confirm.value) {
         password_error.value = '비밀번호가 일치하지 않습니다';
         password_error.style.display = 'block';
         return;
     }
 
     // check personality
-    const personalities = document.querySelectorAll('personality');
+    const personalities = document.querySelectorAll('li');
     let selected_personalities = [];
     let selected_cnt = 0;
     for(let i = 0; i < personalities.length; i++) {
-        if(personalities[i].classList.contains('selected')) {
+        if(personalities[i].classList.contains('selected_keyword_red')) {
             selected_personalities.push(1);
             selected_cnt++;
         }
