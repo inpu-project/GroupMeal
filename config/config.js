4304328e2eb78a5c1c607324bd9b71228a359cbb
danielredglasses <sk091204@gmail.com>
2023-06-10 06:25:41 +0900

    logout fixed

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     "development": {
         "username": "root",
         "password": process.env.DB_PASSWORD,
-        "database": "groupmeal",
+        "database": "GroupMeal",
         "host": "127.0.0.1",
         "dialect": "mysql"
       },
